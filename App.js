d65fcc835cc0a9f34a906c89bbdfc86f9ca14ca8
kim-a-reum <96907807+kim-a-reum@users.noreply.github.com>
2022-08-04 18:18:56 +0900

    8/4

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.buttonText}>To Do</Text>
+        <Text style={styles.buttonText}>Travel</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +16,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    paddingHorizontal: 20,
+  },
+  header: {
+    flexDirection: row,
+    marginTop: 100,
+  },
+  buttonText: {
+    color: white,
   },
 });
