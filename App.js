6bab789c7920be738f58a7a2782cfdb47d6bff58
kim-a-reum <96907807+kim-a-reum@users.noreply.github.com>
2022-08-10 20:41:48 +0900

    8/10

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -7,23 +8,57 @@ import {
   TouchableHighlight,
   TouchableWithoutFeedback,
   Pressable,
+  TextInput,
 } from "react-native";
 import { theme } from "./colors";
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const work = () => setWorking(true);
+  const travel = () => setWorking(false);
+  const onChangeText = (data) => {
+    setText(data);
+  };
+  const addToDo = () => {
+    if (text === "") return;
+    setText("");
+  };
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
-        <TouchableHighlight
-          onPress={() => console.log("눌렸다")}
-          underlayColor="#DDDDDD"
-        >
-          <Text style={styles.btnText}>Work</Text>
-        </TouchableHighlight>
-        <TouchableOpacity activeOpacity={0.3}>
-          <Text style={styles.btnText}>Travel</Text>
+        <TouchableOpacity activeOpacity={0.3} onPress={work}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: working ? "white" : theme.lightgrey,
+            }}
+          >
+            Work
+          </Text>
         </TouchableOpacity>
+        <TouchableOpacity activeOpacity={0.3} onPress={travel}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: working ? theme.lightgrey : "white",
+            }}
+          >
+            Travel
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TextInput
+          onChangeText={onChangeText}
+          value={text}
+          style={styles.TextInput}
+          placeholderTextColor="darkblue"
+          placeholder={working ? "Add to do ! " : "Where do U want to go ? "}
+          onSubmitEditing={addToDo}
+          returnKeyType="done"
+        />
       </View>
     </View>
   );
@@ -44,5 +79,13 @@ const styles = StyleSheet.create({
     color: theme.lightgrey,
     fontSize: 40,
     fontWeight: "900",
+  },
+  TextInput: {
+    backgroundColor: "white",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginTop: 20,
+    borderRadius: 15,
+    fontSize: 20,
   },
 });
